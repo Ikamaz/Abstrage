bce2922a556acc97a0cefaff398f86ef2b3f1be3
Ikamaz <mazmishviliika@gmail.com>
2025-01-29 23:50:44 +0400

    commited

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -1,13 +1,13 @@
 <section class="hero">
     <div class="hero-carousel">
         <div class="carousel-slide">
-            <img src="img/image3.jpg" alt="Hero Image 1">
+            <img src="img/IMG_5695.jpeg" alt="Hero Image 1">
         </div>
         <div class="carousel-slide">
-            <img src="img/image2.avif" alt="Hero Image 2">
+            <img src="img/IMG_4578.jpeg" alt="Hero Image 2">
         </div>
         <div class="carousel-slide">
-            <img src="img/image1.avif" alt="Hero Image 3">
+            <img src="img/IMG_4999.jpeg" alt="Hero Image 3">
         </div>
     </div>
     <div class="hero-text">
