e2addeb41affa78ca7b119b6a31170382ebc76e4
Ikamaz <mazmishviliika@gmail.com>
2025-01-31 23:55:40 +0400

    commitss

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -17,7 +17,6 @@
     <title>ინვოისი</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
             background-color: #f4f4f4;
             margin: 0;
             padding: 20px;
